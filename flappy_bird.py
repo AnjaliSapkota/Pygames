76a503fc2ab8d15a70ed464b1bd63ff0b9493343
AnjaliSapkota <sapkotaanjali123@gmail.com>
2026-06-10 22:31:01 +0545

    ft: bird jump

--- flappy_bird.py
+++ flappy_bird.py
@@ -4,6 +4,27 @@ pygame.init()
 
 screen = pygame.display.set_mode((500, 500))
 
+clock = pygame.time.Clock()
+
+# Colors
+BLUE = (135, 206, 235)
+YELLOW = (255, 200, 0)
+
+# Bird settings
+BIRD_R = 20
+
+
+def draw_bird(surface, x, y):
+    pygame.draw.circle(surface, YELLOW, (x, y), BIRD_R)
+
+
+x = 50
+y = 250
+velocity = 0
+
+# x = 0
+
+
 running = True
 while running:
 
@@ -11,14 +32,23 @@ while running:
         if event.type == pygame.QUIT:
             running = False
      
-        screen.fill((0, 255, 0))           
-    # pygame.draw.rect(
-    # screen,
-    # (255,0,0),
-    # (800,100,50,80)
-# )
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                velocity = -6   
+
+
+    velocity += 0.3   # gravity
+    y += velocity
+
+    
+    screen.fill(BLUE)
+
+    # DRAW
+    draw_bird(screen, x, int(y))
 
     pygame.display.flip()
+
+    clock.tick(60)
 
 
 pygame.quit()
